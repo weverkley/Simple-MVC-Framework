72c952e8a1a3ae638ebe6b717429339648c7465e
wever kley <wever kley>
2017-02-14 15:21:40 -0200

    Adding class for handling json encode and decode

---
+++ library/core/JsonHandler.class.php
@@ -0,0 +1,44 @@
+<?php
+class JsonHandler {
+
+    protected static $_messages = array(
+        JSON_ERROR_NONE => 'No error has occurred',
+        JSON_ERROR_DEPTH => 'The maximum stack depth has been exceeded',
+        JSON_ERROR_STATE_MISMATCH => 'Invalid or malformed JSON',
+        JSON_ERROR_CTRL_CHAR => 'Control character error, possibly incorrectly encoded',
+        JSON_ERROR_SYNTAX => 'Syntax error',
+        JSON_ERROR_UTF8 => 'Malformed UTF-8 characters, possibly incorrectly encoded'
+    );
+
+    public static function encode($status, $message, $data = null, $exception = null) {
+        header("Content-type: application/json; charset=utf-8");
+
+        $array = array(
+            'status' => $status,
+            'message' => $message,
+            'data' => $data,
+            'exception' => $exception
+        );
+
+        $result = json_encode($array);
+
+        if($result)  {
+            return $result;
+        }
+
+        throw new RuntimeException(self::$_messages[json_last_error()]);
+    }
+
+    public static function decode($json, $assoc = false) {
+        header("Content-type: application/json; charset=utf-8");
+
+        $result = json_decode($json, $assoc);
+
+        if($result) {
+            return $result;
+        }
+
+        throw new RuntimeException(self::$_messages[json_last_error()]);
+    }
+
+}
